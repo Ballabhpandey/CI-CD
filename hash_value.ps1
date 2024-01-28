bac23ba61084383684bddf37e3eb6d0009992871
Ballabhpandey <123534066+Ballabhpandey@users.noreply.github.com>
2024-01-28 12:09:55 +0530

    Create hash_value.ps1

---
+++ hash_value.ps1
@@ -0,0 +1,35 @@
+
+$JrxmlPath = "$Env:BUILD_SOURCESDIRECTORY"
+$jsonPath = "$Env:BUILD_SOURCESDIRECTORY/application1.json"
+$hashFolderPath = "$Env:BUILD_SOURCESDIRECTORY/CombinedFiles"
+$repopath = "E:/WEL/CombinedFiles"
+
+# Launch PowerShell with elevated privileges
+Start-Process powershell.exe -Verb RunAs -ArgumentList "-File '$JrxmlPath/script.ps1'"
+
+
+# Get all JRXML files in the specified directory
+$jrxmlFiles = Get-ChildItem -Path $JrxmlPath -Filter *.jrxml
+
+# Create a folder to store all files
+$combinedFolderPath = Join-Path -Path $JrxmlPath -ChildPath "CombinedFiles"
+New-Item -ItemType Directory -Path $combinedFolderPath -Force
+
+# Calculate and create hash files for each JRXML file
+foreach ($file in $jrxmlFiles) {
+    $hash = Get-FileHash -Path $file.FullName -Algorithm SHA256
+    $hashFileName = Join-Path -Path $repopath -ChildPath "$($file.BaseName)_hash.txt"
+    Set-Content -Path $hashFileName -Value $hash.Hash
+    Write-Output "File: $($file.Name), Hash: $($hash.Hash), Hash File: $($hashFileName)"
+}
+cd E:/WEL
+pwd
+git --version
+git init 
+git fetch origin test
+git pull origin test
+git status
+git add CombinedFiles/CardPayMoneyInMetering_hash.txt
+git commit -m "vc"
+
+
