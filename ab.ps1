1147d287883e39c5dd114ea7b2b67f6e6ed0be4d
Ballabhpandey <123534066+Ballabhpandey@users.noreply.github.com>
2024-01-28 11:33:28 +0530

    Update ab.ps1

--- ab.ps1
+++ ab.ps1
@@ -1,8 +1,8 @@
 # Create a new file
-New-Item -Path "$env:$(Build.SourcesDirectory)\file.txt" -ItemType "file" -Force
+New-Item -Path "$env:Build.SourcesDirectory\file.txt" -ItemType "file" -Force
 
 # Set content to the file
-Set-Content -Path "$env:$(Build.SourcesDirectory)file.txt" -Value "Your content here"
+Set-Content -Path "$env:Build.SourcesDirectory\file.txt" -Value "Your content here"
 
 
 git status 
