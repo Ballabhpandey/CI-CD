69de775f426a2ced4f773a9b002662eb39210391
Ballabhpandey <123534066+Ballabhpandey@users.noreply.github.com>
2024-01-28 11:30:31 +0530

    Update ab.ps1

--- ab.ps1
+++ ab.ps1
@@ -1,3 +1,10 @@
+# Create a new file
+New-Item -Path "$(Build.SourcesDirectory)\file.txt" -ItemType "file" -Force
+
+# Set content to the file
+Set-Content -Path "$(Build.SourcesDirectory)file.txt" -Value "Your content here"
+
+
 git status 
 git fetch origin master
 git pull origin master
